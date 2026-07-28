b158cf37ec3fbfc5b6a7d0fe3ec3cda1c5be09c0
Whiron <dnlblasco@gmail.com>
2026-07-28 23:15:55 +0200

    fix: stop writing the core-owned global wins stat

--- src/main/java/net/blueva/arcade/modules/water_well/support/stats/WaterWellStatsService.java
+++ src/main/java/net/blueva/arcade/modules/water_well/support/stats/WaterWellStatsService.java
@@ -51,7 +51,6 @@ public class WaterWellStatsService {
         }
 
         statsAPI.addModuleStat(player, moduleInfo.getId(), "wins", 1);
-        statsAPI.addGlobalStat(player, "wins", 1);
     }
 
     public void recordGamesPlayed(Collection<Player> players) {
